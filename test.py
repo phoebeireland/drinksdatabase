8b6d4416c9c40ba91d7cc9c9af0bec335f488ecb
Phoebe Ireland <pireland2016@gmail.com>
2020-12-15 16:20:29 +0000

    make the table

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("hello")
+import sqlite3
+
+conn = sqlite3.connect("drinksdatabase.db") # or use :memory: to put it in RAM
+
+cursor = conn.cursor()
+
+# create a table
+cursor.execute("""CREATE TABLE drinks
+                  (drink name text, drink type text, drink flavour text, 
+                   drink picture text, yes/no/meh text) 
+               """)
+
+# insert some data
+cursor.execute("INSERT INTO drinks VALUES ('Fanta', 'Soda', 'Shokata', 'Blue bottle with twisted base', 'meh')")
+# save data to database
+conn.commit()
